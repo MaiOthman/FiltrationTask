b7809fd5eb8937bbcfeb5867a10c8495ed9e306b
MaiOthman <mai.m.othman94@gmail.com>
2025-04-22 21:25:07 +0200

    Initial Commit

---
+++ FiltrationTask/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FiltrationTask
+//
+//  Created by Mai Mahmoud on 22.04.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
